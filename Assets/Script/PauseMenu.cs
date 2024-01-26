5549a42eee15e5cd10e66ab83af3e09c3e7928c0
XaSpartanGhost <43203570+XaSpartanGhost@users.noreply.github.com>
2024-01-26 22:14:17 +0100

    PauseMenu with canvas (funcionality only)

---
+++ Assets/Script/PauseMenu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeReference] GameObject pauseMenu;
+
+    // Llamado cuando pausamos el juego
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    // Llamado cuando reanudamos el juego
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    // Llamado cuando salímos del juego al menú principal
+    public void ReturnToMain()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+        Time.timeScale = 1;
+
+    }
+}
