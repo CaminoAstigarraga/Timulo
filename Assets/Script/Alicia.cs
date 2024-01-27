006242c2a54c700ad9fbdffec321e84d7cc1a5f0
CaminoAstigarraga <152734748+CaminoAstigarraga@users.noreply.github.com>
2024-01-27 19:23:23 +0100

    Alicia Terminada

--- Assets/Script/Alicia.cs
+++ Assets/Script/Alicia.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,8 +7,7 @@ public class Alicia : MonoBehaviour
 {
     public Animator aliceAnimator;
 
-    public bool YesPass;
-    public bool NoPass; 
+    
 
     // Start is called before the first frame update
     void Start()
@@ -16,8 +16,29 @@ public class Alicia : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A))
+        {
+            StartCoroutine(AliciaYes()); 
+        }
     }
+   public IEnumerator AliciaYes()
+    {
+        aliceAnimator.SetBool("YesPass", true);
+        yield return new WaitForEndOfFrame();
+        aliceAnimator.SetBool("YesPass", false); 
+      
+    }
+    
+   public IEnumerator AliciaNo()
+    {
+        aliceAnimator.SetBool("NoPass", true);
+        yield return new WaitForSeconds(2);
+        aliceAnimator.SetBool("NoPass", false);
+        yield return null;
+    }
+
+  
 }
