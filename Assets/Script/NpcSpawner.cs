d4e2d3a23d081ca3048632436540bdf23e4e7b23
XaSpartanGhost <43203570+XaSpartanGhost@users.noreply.github.com>
2024-01-28 09:22:28 +0100

    Prefab corrections

--- Assets/Script/NpcSpawner.cs
+++ Assets/Script/NpcSpawner.cs
@@ -19,14 +19,27 @@ public class NpcSpawner : MonoBehaviour
     
     private string generateRandomCharacter()
     {
-        string id = "000";
+        string id = "";
 
         //type
+        id += Random.Range(0, 4);
 
+        //deck
+        if(id.Equals("0"))
+            id += Random.Range(0, 5);
+        else
+            id += 0;
+
+        //hat
+        id += 0;
 
         // glasses
-        
-        return id + Random.Range(0, 2);
+        if (id.Equals("100"))
+            id += 0;
+        else
+            id += Random.Range(0, 2);
+
+        return id;
     }
 
     // Start is called before the first frame update
@@ -40,22 +53,26 @@ public class NpcSpawner : MonoBehaviour
         GameObject firstNpc = Instantiate((GameObject)Resources.Load(aux), SpawnPoint1, Quaternion.identity);
         queue.Add(firstNpc);
         firstNpc.GetComponent<FABRICIANPC>().setInitialPosition(SpawnPoint0, SpawnPoint1);
+        firstNpc.GetComponent<SpriteRenderer>().sortingOrder = 1;
 
         aux = "Prefabs/" + generateRandomCharacter();
         GameObject secondNpc = Instantiate((GameObject)Resources.Load(aux), SpawnPoint2, Quaternion.identity);
         queue.Add(secondNpc);
         secondNpc.GetComponent<FABRICIANPC>().setInitialPosition(SpawnPoint1, SpawnPoint2);
+        secondNpc.GetComponent<SpriteRenderer>().sortingOrder = 2;
 
         aux = "Prefabs/" + generateRandomCharacter();
         GameObject thirdNpc = Instantiate((GameObject)Resources.Load(aux), SpawnPoint3, Quaternion.identity);
         queue.Add(thirdNpc);
         thirdNpc.GetComponent<FABRICIANPC>().setInitialPosition(SpawnPoint2, SpawnPoint3);
+        thirdNpc.GetComponent<SpriteRenderer>().sortingOrder = 3;
 
         aux = "Prefabs/" + generateRandomCharacter();
         GameObject fourthNpc = Instantiate((GameObject)Resources.Load(aux), SpawnPoint4, Quaternion.identity);
         queue.Add(fourthNpc);
         fourthNpc.GetComponent<FABRICIANPC>().setInitialPosition(SpawnPoint3, SpawnPoint4);
-        
+        fourthNpc.GetComponent<SpriteRenderer>().sortingOrder = 4;
+
     }
 
     // Creamos un npc, le pasamos su ubicación inicial, 
@@ -65,6 +82,7 @@ public class NpcSpawner : MonoBehaviour
         GameObject newNpc = Instantiate((GameObject)Resources.Load(aux), SpawnPoint0, Quaternion.identity);
         queue[0] = newNpc;
         newNpc.GetComponent<FABRICIANPC>().setInitialPosition(SpawnPoint0, SpawnPoint1);
+        newNpc.GetComponent<SpriteRenderer>().sortingOrder = 1;
     }
 
 
@@ -85,8 +103,11 @@ public class NpcSpawner : MonoBehaviour
     {
 
         queue[1].GetComponent<FABRICIANPC>().updateTarget(SpawnPoint2);
+        queue[1].GetComponent<SpriteRenderer>().sortingOrder = 2;
         queue[2].GetComponent<FABRICIANPC>().updateTarget(SpawnPoint3);
+        queue[2].GetComponent<SpriteRenderer>().sortingOrder = 3;
         queue[3].GetComponent<FABRICIANPC>().updateTarget(SpawnPoint4);
+        queue[3].GetComponent<SpriteRenderer>().sortingOrder = 4;
 
     }
 
